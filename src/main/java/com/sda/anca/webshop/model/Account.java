e54d9a737142123aea521f1817a03757300d4aa9
Anca <ancaelena.dragomir@gmail.com>
2021-06-06 13:06:45 +0300

    Add first spring boot data jpa

--- src/main/java/com/sda/anca/webshop/model/Account.java
+++ src/main/java/com/sda/anca/webshop/model/Account.java
@@ -1,12 +1,26 @@
 package com.sda.anca.webshop.model;
 
+
+
+import javax.persistence.*;
 import java.util.Date;
+@Entity
+@Table(name="account")
 
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name ="id")
     private Long id; //clasa Long
+
+    @Column(name="billing_address")
     private String billingAddress;
+    @Column(name = "is_closed")
     private boolean isClosed;
+    @Column (name = "creation_date")
     private Date creationDate;
+    @Column(name = "closed_date")
     private Date closedDate;
 
     public Long getId() {
